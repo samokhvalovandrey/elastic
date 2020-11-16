b8cbcac3147e2ded02bddded1a868f8542c8dce3
Samokhvalov <samokhvalov.andrey@gmail.com>
2020-11-16 22:41:22 +0200

    with express commit for deploy

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const deamon = new Deamon();
       deamon.start()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('<h1>TEST APP FOR RESEND DATA FROM RSS ENDPOINTS TO ELASTICSEARCH SERVER</h1>')
 })
  
 app.listen(port, () => console.log(`HelloNode app listening on port ${port}!`));
